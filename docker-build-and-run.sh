17e9b52f7fc8d4bc94706573718ff8737154b9a9
Your Name <you@example.com>
2023-08-18 18:06:54 +0000

    fix echo

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -21,7 +21,7 @@ command=${@:3}
 tag="tempi_$RANDOM$RANDOM"
 name="tempc_$RANDOM$RANDOM"
 trap "docker rm -f $name &> /dev/null; docker rmi -f $tag &>/dev/null" EXIT
-echo docker build -f $dockerfile -t $tag -q $build_context
+echo docker build -f $dockerfile -t $tag $build_context
 docker build -f $dockerfile -t $tag $build_context
 echo $'\n\n\n'
 echo docker run --rm --hostname=dbr_temp --name=$name -it $tag $command
