9f2fbdf3a7b5e0c94f0d52d7ec74422c54484412
Your Name <you@example.com>
2023-08-18 17:14:05 +0000

    working script

---
+++ docker-build-and-run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# if args = -h or --help, print usage
+if [[ $# -eq 1 && ($1 == "-h" || $1 == "--help") ]]; then
+    echo "
+usage: dbr <dockerfile=DOCKERFILE> <build-context-dir=.> <command>...
+example: dbr
+example: dbr my.Dockerfile ./my-build-dir ls -l
+https://github.com/qpwo/docker-build-and-run
+"
+    exit 0
+fi
+
+# dockerfile defaults to "Dockerfile"
+dockerfile=${1:-Dockerfile}
+# build context defaults to current directory
+build_context=${2:-.}
+command=${@:3}
+
+tag="tempi_$RANDOM$RANDOM"
+name="tempc_$RANDOM$RANDOM"
+trap "docker rm -f $name &> /dev/null; docker rmi -f $tag &>/dev/null" EXIT
+echo docker build -f $dockerfile -t $tag -q $build_context
+docker build -f $dockerfile -t $tag $build_context
+echo $'\n\n\n'
+echo docker run --rm --hostname=dbr_temp --name=$name -it $tag $command
+docker run --rm --hostname=dbr_temp --name=$name -it $tag $command
